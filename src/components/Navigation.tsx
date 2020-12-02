4d8f7d0a535a816d5644cf75b4e378a3241e3d06
Mourad Bougarne <contact@mbougarne.me>
2020-12-02 12:15:57 +0100

    Update Navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Icon from './Icon';
 
 const Navigation = () => {
 	return (
@@ -11,14 +12,11 @@ const Navigation = () => {
 				className="navbar-brand font-weight-bold">
 				MR
 			</NavLink>
+
 			<button 
 				className="navbar-toggler" 
 				type="button" 
-				data-toggle="collapse" 
-				data-target="#navbarCollapse" 
-				aria-controls="navbarCollapse" 
-				aria-expanded="false" 
-				aria-label="Toggle navigation">
+			>
 				<span className="navbar-toggler-icon"></span>
 			</button>
 
@@ -62,12 +60,13 @@ const Navigation = () => {
 						</NavLink>
 					</li>
 				</ul>
-
-				<div className="form-inline mt-2 mt-md-0">
-					<input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-					<button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-				</div>
-
+			</div>
+			<div className="mt-2 mt-md-0">
+				<button 
+					className="btn btn-search"
+				>
+					<Icon name="search" />
+				</button>
 			</div>
   	</nav>
 	)
