c420e67b2b533e380727e88d2cf24f02ab742933
Mourad Bougarne <contact@mbougarne.me>
2020-12-02 23:31:40 +0100

    Add redux to PopularShows page

--- src/pages/PopularShows.tsx
+++ src/pages/PopularShows.tsx
@@ -1,60 +1,64 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { popularShows } from '../store/actions';
 
 import Show from '../components/Show';
+import Error from '../components/Error';
 
-import configs from '../configs';
 import ShowSchema from '../types/Show';
+import StateSchema from '../types/State';
 
-export default class PopularShows extends Component<any, any>
+class PopularShows extends Component<any, any>
 {
   constructor(props: {} | Readonly<{}>)
   {
     super(props);
-    this.state = {
-      shows: []
-    }
-  }
-
-  moviesLink() : string
-  {
-    return configs.BASE_URL + 'tv/popular?api_key=' + configs.API_KEY + '&language=en-US&page=1';
-  }
-
-  async fetchShows()
-  {
-    try {
-
-      const res = await fetch(this.moviesLink());
-      const data = await res.json()
-      
-      this.setState((state: any) => ({
-        shows: [...state.shows, data.results]
-      }))
-      
-    } catch (error) {
-      console.log(error) 
-    }
   }
 
   componentDidMount()
   {
-    this.fetchShows();
+    this.props.popularShows();
   }
 
   render()
   {
-    let { shows } = this.state;  
-    let Shows = (shows.length > 0) ? shows[0].map((item: ShowSchema) => <Show show={item} key={item.id} />) : '';
+    let { shows, error, errorMessage, loaded } = this.props;  
+    let Shows = (shows.length > 0) ? shows.map((item: ShowSchema) => <Show show={item} key={item.id} />) : '';
+
+    const UI = () => {
+      if(error)
+      {
+        return <Error title="404 Not Found" message={errorMessage} />
+      } else if (!loaded) {
+        return <h1>LOADING DATA...</h1>
+      }
+
+      return (
+        <>
+          <div className="my-3 py-2">
+            <h1 className="mb-4 text-uppercase font-weight-bold">Top Rated TV Shows</h1>
+            <div className="row">
+              {Shows}
+            </div>
+          </div>
+        </>
+      )
+    }
 
     return (
       <div className="PopularShows container-fluid">
-        <h1>Top Rated TV Shows</h1>
-        <div className="my-3 py-2">
-          <div className="row">
-            {Shows}
-          </div>
-        </div>
+        <UI />
       </div>
     )
   }
 }
+
+const mapStateToProps = (state: StateSchema) => ({
+  shows: state.popularShows,
+  error: state.isError,
+  errorMessage: state.message,
+  loaded: state.isLoaded
+});
+
+
+export default connect(mapStateToProps, { popularShows })(PopularShows);
--- src/store/actions/creators/popularShows.ts
+++ src/store/actions/creators/popularShows.ts
@@ -2,7 +2,7 @@ import MVDB from '../../../configs';
 
 import { IS_ERROR, LOAD_POPULAR_SHOWS } from '../types';
 
-const popularShows = async (dispatch: any) => {
+const popularShows = () => async (dispatch: any) => {
 
   const link = MVDB.BASE_URL + 'tv/popular?api_key=' + MVDB.API_KEY + '&language=en-US&page=1';
   const res = await fetch(link);
