798d410e686395170cc6b7b8d12474209b671002
Mourad Bougarne <contact@mbougarne.me>
2020-12-02 12:54:59 +0100

    Create Search component

---
+++ src/components/Search.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Search()
+{
+  return (
+    <div className="SearchPage">
+      <div className="overly"></div>
+      <div className="container">
+        {/* Search Content */}
+      </div>
+    </div>
+  )
+}
